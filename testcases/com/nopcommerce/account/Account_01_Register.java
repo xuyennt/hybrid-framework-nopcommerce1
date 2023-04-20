7074d7c45cbd053bee49d1400f63e1edc2e5c168
DESKTOP-1BTBBPR\Admin <Xuyennt6>
2023-04-20 14:50:32 +0700

    framework nopcommerce

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,32 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Account_01_Register {
+  @Test
+  public void Register_01_Empty_Data() {
+  }
+  @Test
+  public void Register_02_Invalid_Email() {
+  }
+  @Test
+  public void Register_03_Invalid_Password() {
+  }
+  
+  @Test
+  public void Register_04_Incorrect_Confirm_Password() {
+  }
+  @Test
+  public void Register_05_Success() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
